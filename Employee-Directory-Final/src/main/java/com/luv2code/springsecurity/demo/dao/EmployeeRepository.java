1ac617b12604b7fbcdf450a32121e7aee5a5086b
Divs2001 <divyanshjpr01@gmail.com>
2021-07-29 16:28:48 +0530

    First Commit of Employee Directory Manager

---
+++ Employee-Directory-Final/src/main/java/com/luv2code/springsecurity/demo/dao/EmployeeRepository.java
@@ -0,0 +1,20 @@
+package com.luv2code.springsecurity.demo.dao;
+
+import java.util.List;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springsecurity.demo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+	
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
+	
+	// search by name
+	public List<Employee> findByFirstNameContainsOrLastNameContainsAllIgnoreCase(String name, String lName);
+
+}
